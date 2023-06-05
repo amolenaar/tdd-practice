b7624e6e2111ddaff01534a6642d1692a7fa9492
Arjan Molenaar <gaphor@gmail.com>
2023-06-05 12:52:51 +0200

    A basic Maven project, generated by IntelliJ

---
+++ src/test/java/org/example/RomanNumberTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class RomanNumberTest
+    extends TestCase
+{
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( RomanNumberTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
